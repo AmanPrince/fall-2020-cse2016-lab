1a3c0b8ceae772042c441fdf025047b65e275cfd
mridul018 <mridul.link@gmail.com>
2021-01-06 03:00:47 +0600

    do some work

--- src/sample/utils/CommonConstant.java
+++ src/sample/utils/CommonConstant.java
@@ -4,7 +4,7 @@ public class CommonConstant {
 
     public static final String DB_NAME="PreRegistration_Spring2021";
     public static final String MYSQL_USERNAME = "root";
-    public static final String MYSQL_PASSWORD = "mmmmmm018";
+    public static final String MYSQL_PASSWORD = "";
     public static final String IP_ADDRESS = "localhost"; // no change
     public static final int PORT = 3306;
 }
